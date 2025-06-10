3b223470ab2c17d9d9ded9cf503011c3b89bc027
Maycon F. Melo <30307203+mayconfmelo@users.noreply.github.com>
2025-06-10 13:39:40 -0300

    upd: argparse arguments, continuous building mode rewriten, and other major changes

--- src/bookmkr.py
+++ src/bookmkr.py
@@ -2,9 +2,6 @@
 # DESC: Build Pandoc books based on instructions in bookrecipe.toml files.
 # USAGE: bookmkr [OPTIONS] <FORMAT>
 
-# TODO: Implement min-book commands in md files through Perl filters
-
-
 import yaml
 import glob
 import os
@@ -16,29 +13,24 @@ import file
 # CLI argument parsing
 parser = argparse.ArgumentParser(description="")
 parser.add_argument(
-    "-v", "--verbose", 
-    help="enables verbose mode.", action="store_true"
-)
-parser.add_argument(
-    "-l", "--loop", const=1.0,
-    help="enables infinite loop.", type=float, nargs='?'
-)
-parser.add_argument(
-    "-s", "--sleep-time",
-    help="set time between each build in loop mode (default: 1.0)",
-    type=float, default=1.0
+    "format",
+    help="set the book format.", nargs="?",
 )
 parser.add_argument(
     "-i", "--init",
     help="initialize a new book project.", action="store_true",
 )
 parser.add_argument(
-    "-c", "--color", default=True,
-    help="initialize a new book project.", action=argparse.BooleanOptionalAction
+    "-w", "--watch",
+    help="enables infinite loop.", action='store_true',
 )
 parser.add_argument(
-    "format",
-    help="set the book format.", nargs="?",
+    "-v", "--verbose", 
+    help="enables verbose mode.", action="store_true"
+)
+parser.add_argument(
+    "-c", "--color", default=True,
+    help="set colored text status.", action=argparse.BooleanOptionalAction
 )
 # CLI arguments
 args = parser.parse_args()
@@ -56,11 +48,13 @@ cfg_file = 'bookrecipe.toml'
 cfg_default = proj_dir + "/assets/" + cfg_file
 cfg_local = file.find(cfg_file)
 
-if cfg_local: os.chdir(os.path.dirname(cfg_local))
 
 # Init a new book
 if args.init:
-    log = utils.Log(verbose=True)
+    log = utils.Log(
+        verbose=True,
+        color=args.color
+    )
     
     if not cfg_local:
         import shutil
@@ -99,12 +93,13 @@ if args.init:
     else:
         log.w("There's a book project here already!")
         exit(1)
+ 
+ 
+if cfg_local:
+    cfg_local_dir = os.path.dirname(cfg_local)
+    os.chdir(cfg_local_dir)
+else: log.f("Not a book project", code=2)
 
-
-# Abort if not in a book project directory
-if not cfg_local:
-    log.f("Not a book project")
-    exit(2)
 
 
 # Book configurations
@@ -117,12 +112,13 @@ if args.format: cfg.general.format = args.format
 # Write .data.yaml file in assets/
 file.write(
   yaml.dump(cfg["book"]),
-  os.path.dirname(cfg_local) + "/assets/.data.yaml"
+  cfg_local_dir + "/assets/.data.yaml"
 )
 cfg.book.title = cfg.book.title.replace("\\n", " ")
+cfg.general.sources = file.globs(cfg.general.sources)
 
-log.m("Book title:", f"{cfg.book.title}")
-log.m("Book format:", f"{cfg.general.format}")
+log.m("Book title:", cfg.book.title)
+log.m("Book format:", cfg.general.format)
 
 
 # Collect CLI flags/arguments
@@ -135,7 +131,7 @@ for key, value in cfg.pandoc.flags.items():
 
 # Get the relative output directory
 output_dir = os.path.join(
-    os.path.relpath(os.path.dirname(cfg_local)),
+    os.path.relpath(cfg_local_dir),
     cfg.general.output
 )
 if not os.path.isdir(output_dir): os.mkdir(output_dir)
@@ -148,62 +144,98 @@ output = os.path.join(
 )
 
 # Generates the pandoc command:
+command = 'a'
 command = 'pandoc '
 command += f'--output="{output}" '
 command +=  '--metadata-file="assets/.data.yaml" '
 command += f'{flags}'
 #command += " ".join(sorted( glob.glob(cfg.general.sources) ))
-command += " ".join(file.globs(cfg.general.sources))
+command += " ".join(cfg.general.sources)
 
-if args.loop:
-    import time
-    log.m(f"Continuous building mode ({args.sleep_time}s)")
+# if args.watch:
+#     import time
+#     log.m(f"Continuous building mode ({args.sleep_time}s)")
 
-while True:
+
+# Run cmd-before, command, and cmd-after
+def execute():
+    # End "Waiting for for changes" with DONE in loop
+    if args.watch: print("DONE")
+  
     # Optional command executed before pandoc
     if cfg.general.get("cmd-before"):
         cmd_before = cfg.general["cmd-before"]
         
         log.w("Running pre-generation command:", cmd_before)
-        
-        os.chdir(os.path.dirname(cfg_local))
-        
+        os.chdir(cfg_local_dir)
         out = utils.run(cmd_before, get_output=True)
+        
         if args.verbose and out != '': 
             utils.pad(out, wraplines=False)
             print()
     
-    
-    log.o("Building book...", )
+    # Verbose changes between bookmkr and bookmkr --watch
+    if args.watch:
+        from datetime import datetime
+        now = f": {datetime.now().strftime("%H:%M:%S")}"
+        points = "........."
+    else:
+        now = ""
+        points = "..."
+        
+    log.o(f"Building book" + points)
     
     # Execute the pandoc command
     utils.run(command)
-    if args.verbose and not args.loop: print("OK\n")
     
+    if args.verbose or args.watch: print(f"DONE{now}\n")
+    if args.watch: log.o("Waiting for changes...")
     
     # Optional command executed after pandoc
     if cfg.general.get('cmd-after'):
         cmd_after = cfg.general['cmd-after']
         
         log.w("Running post-generation command:", cmd_after)
-        
-        os.chdir(os.path.dirname(cfg_local))
-        
+        os.chdir(cfg_local_dir)
         out = utils.run(cmd_after, get_output=True)
+
         if args.verbose and out != '':
           utils.pad(out, wraplines=False)
           print()
-    
-    
-    # Sleep (loop) or end the program
-    if args.loop:
-        try: time.sleep(args.loop)
-        
-        
-        except KeyboardInterrupt:
-            log.w("Stoping continuous mode.")
-            exit(0)
-    else: break
 
 
-log.s("Book created:", os.path.relpath(output))
+# When bookmkr --watch
+if args.watch:
+    from watchdog.observers import Observer
+    import time
+    
+    # Init a new log with verbose=True
+    log = utils.Log(
+        verbose=True,
+        color=args.color
+    )
+    
+    log.m(f"Continuous building mode")
+
+    # Event handler runs execute() when a file in cfg.general.sources is modified
+    event_handler = utils.WatchHandler(execute, cfg.general.sources)
+    observer = Observer()
+    observer.schedule(event_handler, cfg_local_dir, recursive=True)
+    observer.start()
+    
+    log.o("Waiting for changes...")
+    # Trap the execution in an infinite loop
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        print()
+        log.w("Stoping continuous mode.")
+        exit(0)
+    observer.join()
+else:
+  execute()
+  
+  if os.path.isfile(output): log.s("Book created:", os.path.relpath(output))
+  else: log.f("Error: No book file was generated.", code=2)

32115eb329dd985fd3213300badf954dc67f8149
Maycon F. Melo <30307203+mayconfmelo@users.noreply.github.com>
2025-06-10 13:27:47 -0300

    added: filters for Div blocks in Typst

--- assets/filters.lua
+++ assets/filters.lua
@@ -2,8 +2,6 @@
 
 -- GitHub-like [!CALLOUT] in blockquotes
 function BlockQuote(el)
-  print(el)
-  
   local content = el.content
   for i, blk in ipairs(content) do
     if blk.t == "Para" and #blk.c > 0 then
@@ -18,4 +16,61 @@ function BlockQuote(el)
     end
   end
   return el
+end
+
+function Div(el)
+  -- Typst
+  if FORMAT == "typst" then
+    -- Parse Div content as Typst code
+    local content = pandoc.write(pandoc.Pandoc(el.content), "typst")
+      :gsub("^%s*", ""):gsub("%s*$", "")
+    
+    -- ::: {.dropcap}
+    if el.classes:includes("dropcap") then
+      content = [[
+        #import "@preview/droplet:0.3.1": dropcap
+        #dropcap(height: 3)[
+      ]] .. content .. "\n]\n"
+    
+    -- ::: {.no-dropcap}
+    elseif el.classes:includes("dropcap") then
+      content = "#block[\n#let dropcap(c) = {c}\n" .. content
+    end
+      
+    -- ::: {.indent}
+    if el.classes:includes("indent") then
+      content = "#context block[\n#set par(first-line-indent: (..par.first-line-indent, all: true))\n" .. content .. "\n]\n"
+  
+    -- ::: {.no-indent}
+    elseif el.classes:includes("no-indent") then
+      content = "#context block[\n#set par(first-line-indent: (amount: 0em, all: true))\n" .. content .. "\n]\n"
+    end
+    
+    -- ::: {.center}
+    if el.classes:includes("center") then
+      content = "#align(center)[\n" .. content .. "\n]\n"
+  
+    -- ::: {.left}
+    elseif el.classes:includes("left") then
+      content = "#align(left)[\n" .. content .. "\n]\n"
+    
+    -- ::: {.right}
+    elseif el.classes:includes("right") then
+      content = "#align(right)[\n" .. content .. "\n]\n"
+    end
+    
+    return pandoc.RawBlock("typst", content)
+  
+  -- Other formats
+  else
+    return nil
+  end
+end
+
+
+function RawInline(el)
+  -- Get \n in Typst
+  if el.format == "tex" and FORMAT == "typst" then
+    return pandoc.RawInline("typst", el.text)
+  end
 end
